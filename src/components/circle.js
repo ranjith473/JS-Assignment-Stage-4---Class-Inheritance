a3ce610ba676784d8f663c73027140c6ea40f48d
ranjith <ranjith.juluru14@gmail.com>
2020-12-05 19:02:31 +0530

    inheritance

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,13 @@
 
 
 // export class using module.exports
+const Shape = require('./shape');
+class Circle extends Shape {
+    constructor() {
+        super();
+    }
+    caculateArea() {
+        console.log("Area of circle with color" + this.color);
+    return 3.14*5*5;
+    }
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+    constructor() {
+        this.color="red";
+    }
+    drawShape() {
+        console.log("Drawing a shape");
+    }
+    caculateArea() {
+        console.log("don't know area of unknown shape");
+        return 0;
+    }
+}
+module.exports = Shape;
